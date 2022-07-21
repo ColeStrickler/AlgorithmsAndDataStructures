c342370c5fbc076edae7639a5a8e710f4ac9f441
Cole Strickler <colestrickler@gmail.com>
2022-07-21 18:03:13 -0500

    final

--- Unit4/graphAlgos/GraphAlgorithms.java
+++ Unit4/graphAlgos/GraphAlgorithms.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
+import java.util.PriorityQueue;
 
 /**
  * Your implementation of various graph traversal algorithms.
@@ -61,4 +62,74 @@ public class GraphAlgorithms {
         dfsRecurse(start, graph, visitedSet);
         return visitedSet;
     }
+
+    public static <T> Set<Edge<T>> prims(Vertex<T> start, Graph<T> graph) {
+        List<Vertex<T>> visitedSetU = new ArrayList<> ();
+        List<Vertex<T>> visitedSetV = new ArrayList<> (); // added second set
+        Set<Edge<T>> mstSet = new HashSet<>();
+        PriorityQueue<Edge<T>> pq = new PriorityQueue<>();
+
+        List<Edge<T>> edgeList = new ArrayList<>();
+
+        Set<Edge<T>> tmp = graph.getEdges();
+        Set<Vertex<T>> vertSet = graph.getVertices();
+
+        for (Edge<T> i : tmp) {
+            edgeList.add(i);
+        }
+
+
+
+
+
+
+        for (int i = 0; i < edgeList.size(); i++) {
+            if (edgeList.get(i).getU().equals(start)) {
+                pq.add(edgeList.get(i));
+                mstSet.add(edgeList.get(i));
+            }
+        }
+
+        visitedSetU.add(start);
+
+        while (!pq.isEmpty()) {
+
+            Edge<T> curr = pq.poll();
+            if (visitedSetV.contains(curr.getV()) == false) {
+                visitedSetV.add(curr.getV());
+                mstSet.add(curr);
+
+                for (int i = 0; i< edgeList.size(); i++) {
+                    if (edgeList.get(i).getU().equals(curr.getV())) { //&& visitedSet.contains(edgeList.get(i).getV()) == false) {
+                        pq.add(edgeList.get(i));
+                    }
+                }
+
+            }
+
+            if (visitedSetU.contains(curr.getU()) == false) {
+                visitedSetU.add(curr.getU());
+                mstSet.add(curr);
+
+                for (int i = 0; i< edgeList.size(); i++) {
+                    if (edgeList.get(i).getV().equals(curr.getU())) { //&& visitedSet.contains(edgeList.get(i).getV()) == false) {
+                        pq.add(edgeList.get(i));
+                    }
+                }
+
+            }
+
+        }
+
+
+
+      
+            return mstSet;
+        
+       
+
+    }
+
+
+
 }
